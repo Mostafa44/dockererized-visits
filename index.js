030d4f2854625920e5e0aa58d7d911d11e87a9f8
mostafa.abdelshafy <mostafa.abdelshafy88@gmail.com>
2021-10-02 12:22:50 +0200

    revert to normal node app with no restarts in the docker file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require( 'express');
 var  redis=  require ('redis');
-var process= require('process');
+
 
 const app= express();
 const redisClient= redis.createClient({
@@ -10,7 +10,7 @@ redisClient.set('visits',0);
 
 
 app.get('/', (req, res)=>{
-    process.exit(0);
+
     redisClient.get('visits', (err,visits)=>{
         res.send(`number of visits is ${visits}`);
         redisClient.set('visits', parseInt(visits)+1);
